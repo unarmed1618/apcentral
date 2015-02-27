eb686587ad6af50483c95b230585a6c83bf337c8
Kalia Jade Darrow <darrowj08@students.ecu.edu>
2015-02-26 17:41:28 -0800

    Adding the correct project >_<

--- web.js
+++ web.js
@@ -1,18 +1,60 @@
 // web.js -- Filename 
 // This section necessary to declare plugins/extensions
-
 var jquery = require("jquery");
+var flash = require("connect-flash")
 var express = require("express");
 var logfmt = require("logfmt");
 var fs = require("fs");
+var forms = require("forms");
+var models = require("./models");
+var mongoose = require("mongoose");
+var mongoStore = require("connect-mongodb")
 var jade = require("jade");
-var jadeOptions = { filename: './', pretty: true };
-
 var url = require("url");
-
+var jadeOptions = { filename: './', pretty: true };
+var Page, Entry, LoginToken, User,db;
 var app = express();
-app.use(express.favicon("favicon.ico"));
+function renderJadeFile(template, options) {
+  var fn = jade.compile(template, options);
+  return fn(options.locals);
+}
+ var dbstr = "mongodb://human:hurpen@ds039860.mongolab.com:39860/heroku_app21751565"
+app.configure('development', function() {
+  app.set('db-uri', dbstr);
+  app.use(express.errorHandler({ dumpExceptions: true }));
+  app.set('view options', {
+    pretty: true
+  });
+});
+
+app.configure('test', function() {
+  app.set('db-uri', dbstr);
+  app.set('view options', {
+    pretty: true
+  });
+});
+
+app.configure('production', function() {
+  app.set('db-uri', dbstr);
+});
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.use(express.cookieParser());
+  app.use(express.session({ store: mongoStore(app.set('db-uri')), secret: 'topsecret' }));
+  app.use(flash());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+});
 //settable application attribute variables
+models.defineModels(mongoose, function() {
+  app.Entry = Entry = mongoose.model('Entry');
+  app.Page = Page = mongoose.model('Page');
+  app.User = User = mongoose.model('User');
+  app.LoginToken = LoginToken = mongoose.model('LoginToken');
+  db = mongoose.connect(app.set('db-uri'));
+
+})
 
 app.locals({
 	title: "Darrow's portfolio application"
@@ -46,78 +88,157 @@ fs.readFile("recentProjectData.json","utf8", function (err, data) {
 app.use(logfmt.requestLogger()); //logfmt hook
 
 //Routing happens from here down
-//app.post('/login',passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login'}));
 
-//app.get('/login')
-
-app.get('/bootstrap.css', function(req,res) {
-	res.writeHead(200, {"Content-Type": "text/css"});
-	fs.readFile('./css/bootstrap.css' , 'utf8', function(err, fd) {
-		res.end(fd);
-	    });
-    });
-app.get('/academicTranscript', function(req,res) {
-	res.writeHead(200, {"Content-Type": "application/pdf"});
-	fs.readFile('./at.pdf', function(err, fd) {
-		res.end(fd);
-	    });
-    });
-app.get('/adaptPaper', function(req,res) {
-	res.writeHead(200, {"Content-Type": "application/pdf"});
-	fs.readFile('./adaptiveEEG.pdf', function(err,fd) {
-		res.end(fd);
-	    });
-    });
-app.get('/resume', function(req,res) {
-	res.writeHead(200, {"Content-Type": "application/pdf"});
-	fs.readFile('./resume.pdf',function(err,fd) {
-		res.end(fd);
-	    });
-    });
-app.get('/bootstrap.css.map',function(req,res) {
-	res.send(fs.readFileSync('css/bootstrap.css.map').toString());
-    });
 app.get('/contactme', function(req,res) {
-	res.send(jcn(contactInfo));	
+//	res.send(jcn(contactInfo));	
+    res.render('contact.jade',contactInfo);
     });
 app.get('/personalinfo', function(req,res){
-	res.send(jpers(personalInfo));
+//	res.send(jpers(personalInfo));
+    res.render('pers.jade',personalInfo);
     });
-app.get('/bootstrap.js',function(req,res){
-	res.writeHead(200, {"Content-Type": "text/javascript"});
-	fs.readFile('js/bootstrap.js', 'utf8', function(err, fd) {
-                res.end(fd);
-            });
+function loadUserPassive(req,res,next) {
+console.log("Entered passiveLoad");
+//console.log(req);
+//console.log(res);
+    if (req&&req.session&&req.session.user_id) {
+        //console.log("Entered If");
+        User.findOne({_id:req.session.user_id}, function(err,user) {
+            if (user) {
+                console.log("Found User");
+                req.currentUser = user;
+                next();
+            }  else {
+                console.log("User not Found");
+                next();
+                }
+});
+} else {
+    console.log("No user logged in");
+    next();
+}
+}
+
+
+/* Implement this -- */
+app.get('/recognize', loadUserPassive, function(req,res){
+if(req.currentUser) {
+res.send("Hello, <a href='/console/"+req.currentUser.shortname +"'>"+ req.currentUser.first_name +".");
+} else {
+res.send("<a href='/login'>Login</a> or <a href='/signup'>Sign up</a>");
+}
+});
+app.get('/console/:userId', loadUserPassive, function(req,res){
+if(req.currentUser) {
+    User.findOne({'shortname':req.params.userId},function(err,user){
+	res.render('console.jade',{'user':user});
+});
+}
+else
+res.redirect('/login');
+});
+app.get('/scan', function(req,res){
+res.render('scanner.jade');
+});
+
+//Add authentication for is this user the userId
+app.get('/:userId/new',function(req,res){
+res.render('entryForm.jade',{'user':req.params.userId});
+//Return a form that creates a new entry for this user
+});
+app.get('/:userId/', loadUserPassive, function(req,res){
+    Page.findOne({'shortname':req.params.userId,'path':'/'},function(err,page) {
+        res.render('splash.jade',page);
     });
+});
 
-//Login info for ManagementConsole
+app.get('/:userId/:path', function(req,res){
+    Page.findOne({'shortname':req.params.userId,'path':req.params.path},function(err,page) {
+	console.log(page);
+        res.render('splash.jade',{'page':page});
+    });
+});
 
-//Respond to :id with userid's :element
+//Add auth
+app.get('/:userId/:path/edit',function(req,res){
+    Page.findOne({'shortname':req.params.userId,'path':req.params.path},function(err,page) {
+        res.render('entryForm.jade',{'user':req.params.userId,'path':req.params.path,'page': page});
+    });
+});
+//add auth
+app.post('/:userId/new', function(req,res){
+var e = new Page(req.body.page);
+e.shortname = req.params.userId;
+e.save();
+res.redirect('/'+req.params.userId+'/'+e.path);
+});
+
+app.post('/:userId/update', function(req,res){
+Page.update({'shortname':req.params.userId,'path':req.body.path},req.body.page,function(err, page,lastErrorObject) {
+res.redirect('/'+req.params.userId+'/'+page.path);
+});
+});
+app.post('/:userId/:path/del', function(req,res){
+Page.findOne({'shortname':req.params.userId,'path':req.body.path},function(err,page) {
+page.remove;
+});
+});
+app.get('/signup', function(req,res){
+//console.log("Signup route works");
+//Return a form that creates a new User.
+res.render('signupForm.jade');
+});
+app.post('/signup',function(req,res){
+//console.log(req.body);
+var u = new User(req.body.user);
+console.log(u);
+u.save();
+//console.log("U has been saved");
+res.redirect('/signup');
+});
+app.get('/login', function(req,res){
+//Create new loginToken -- Port from pirategolf
+res.render('login.jade');
+});
+app.post('/login', function(req, res) {
+  User.findOne({ email: req.body.user.email }, function(err, user) {
+    if (user && user.authenticate(req.body.user.password)) {
+      req.session.user_id = user.id;
+      // Remember me                                               
+      if (true) {
+        var loginToken = new LoginToken({ email: user.email });
+        loginToken.save(function() {
+          res.cookie('logintoken', loginToken.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/console/'+user.shortname });
+          res.redirect('/console/'+user.shortname);
+        });
+      } else {//no remember                      
+        res.redirect('/console/'+user.shortname);
+      }
+    } else {
+      req.flash('error', 'Incorrect credentials');
+      res.redirect('/login');
+    }
+  });
+});
+app.get('/logout', function(req,res){
+req.session.currentUser= null;
+
+//kill session
+});
 /*
-app.get('/:id/', function(req,res) {
-var data = db.get(:id.splash);
-
-res.send(jsp(data));
-});
-
-app.get('/:id/:element' function(req,res) {
-var data = db.get(:id.:element);
-res.send(jsp(data));
-});
-app.get('/mgt/:id', function(req,res) {
-
-
-});
- */
-
+app.use('/:userId/*', function(req,res,next){
+User.findById(req.params.userId);
+});*/
 app.get('/', function(req, res) {
-	res.send(jsp(portfolioData));
+    res.render('splash.jade',portfolioData);
     });
 app.get('/recentProjects', function(req,res) {
-	res.send(jsp(recentProjectData));
+//	res.send(jsp(recentProjectData));
+    res.render('splash.jade',recentProjectData);
     });
 app.get('/earlyProjects', function(req,res) {
-	res.send(jsp(earlyProjectData));
+//	res.send(jsp(earlyProjectData));
+    res.render('splash.jade',earlyProjectData);
     });
 // Don't mess with this stuff
 var port = process.env.PORT || 5000;
