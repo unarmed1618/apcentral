ce9420b6bcc2ce140eac53bf90311b33b703dae2
Emily Jade <unarmed1618@users.noreply.github.com>
2015-03-13 11:44:41 -0400

    Update web.js

--- web.js
+++ web.js
@@ -95,6 +95,39 @@ console.log("Entered passiveLoad");
 }
 }
 
+app.get('/signup', function(req,res){
+    res.render('signupForm.jade');
+});
+app.post('/signup',function(req,res){
+    var u = new User(req.body.user);
+    u.save();
+    res.redirect('/signup');
+});
+app.get('/login', function(req,res){
+    res.render('login.jade');
+});
+app.post('/login', function(req, res) {
+  User.findOne({ email: req.body.user.email }, function(err, user) {
+    if (user && user.authenticate(req.body.user.password)) {
+      req.session.user_id = user.id;
+      if (true) {
+        var loginToken = new LoginToken({ email: user.email });
+        loginToken.save(function() {
+          res.cookie('logintoken', loginToken.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/console/'+user.shortname });
+          res.redirect('/console/'+user.shortname);
+        });
+      } else {//no remember                      
+        res.redirect('/console/'+user.shortname);
+      }
+    } else {
+      req.flash('error', 'Incorrect credentials');
+      res.redirect('/login');
+    }
+  });
+});
+app.get('/logout', function(req,res){
+req.session.currentUser= null;
+});
 
 /* Implement this -- */
 app.get('/recognize', loadUserPassive, function(req,res){
@@ -158,48 +191,6 @@ app.post('/:userId/:path/del', function(req,res){
 Page.findOne({'shortname':req.params.userId,'path':req.body.path},function(err,page) {
 page.remove;
 });
-});
-app.get('/signup', function(req,res){
-    //console.log("Signup route works");
-    //Return a form that creates a new User.
-    res.render('signupForm.jade');
-});
-app.post('/signup',function(req,res){
-    //console.log(req.body);
-    var u = new User(req.body.user);
-    console.log(u);
-    u.save();
-    //console.log("U has been saved");
-    res.redirect('/signup');
-});
-app.get('/login', function(req,res){
-    //Create new loginToken
-    res.render('login.jade');
-});
-app.post('/login', function(req, res) {
-  User.findOne({ email: req.body.user.email }, function(err, user) {
-    if (user && user.authenticate(req.body.user.password)) {
-      req.session.user_id = user.id;
-      // Remember me                                               
-      if (true) {
-        var loginToken = new LoginToken({ email: user.email });
-        loginToken.save(function() {
-          res.cookie('logintoken', loginToken.cookieValue, { expires: new Date(Date.now() + 2 * 604800000), path: '/console/'+user.shortname });
-          res.redirect('/console/'+user.shortname);
-        });
-      } else {//no remember                      
-        res.redirect('/console/'+user.shortname);
-      }
-    } else {
-      req.flash('error', 'Incorrect credentials');
-      res.redirect('/login');
-    }
-  });
-});
-app.get('/logout', function(req,res){
-req.session.currentUser= null;
-
-//kill session
 });
 app.get('/:userId',loadUserPassive,function(req,res){
 	Page.findOne({'shortname':req.params.userId,'path':'/'},function(err,page) {
