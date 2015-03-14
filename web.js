6fc0eea80825594a7adb887d6deb4509cd548ad1
Emily Jade <unarmed1618@users.noreply.github.com>
2015-03-14 01:55:51 -0400

    Update web.js

--- web.js
+++ web.js
@@ -57,7 +57,7 @@ models.defineModels(mongoose, function() {
 
 })
 
-var logins = require('./login')(app);
+//var logins = require('./login')(app);
 app.locals({
 	title: "Darrow's portfolio application"
     });
@@ -75,7 +75,7 @@ app.get('/personalinfo', function(req,res){
 //	res.send(jpers(personalInfo));
     res.render('pers.jade',personalInfo);
     });
-    /*
+    
 function loadUserPassive(req,res,next) {
 console.log("Entered passiveLoad");
 //console.log(req);
@@ -133,7 +133,7 @@ req.session.currentUser= null;
 });
 
 /* Implement this -- */
-/*
+
 app.get('/recognize', loadUserPassive, function(req,res){
 if(req.currentUser) {
 res.send("Hello, <a href='/console/"+req.currentUser.shortname +"'>"+ req.currentUser.first_name +".");
@@ -141,7 +141,7 @@ res.send("Hello, <a href='/console/"+req.currentUser.shortname +"'>"+ req.curren
 res.send("<a href='/login'>Login</a> or <a href='/signup'>Sign up</a>");
 }
 });
-*/
+
 app.get('/console/:userId', loadUserPassive, function(req,res){
 if(req.currentUser) {
     User.findOne({'shortname':req.params.userId},function(err,user){
