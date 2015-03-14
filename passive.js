0ba0f4452f8d56964fe71f3447fb4beb212eaf13
Emily Jade <unarmed1618@users.noreply.github.com>
2015-03-14 01:15:29 -0400

    Update passive.js

--- passive.js
+++ passive.js
@@ -1,4 +1,6 @@
 var models = require("./models");
+var express = require("express");
+var app = express();
 var mongoose = require("mongoose");
 var mongoStore = require("connect-mongodb");
 var passive = require("./passive");
