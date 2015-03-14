4be355c92195a040b870133b4b783703721d0828
Emily Jade <unarmed1618@users.noreply.github.com>
2015-03-14 01:13:27 -0400

    Update login.js

--- login.js
+++ login.js
@@ -2,6 +2,8 @@
 
 var models = require("./models");
 var mongoose = require("mongoose");
+var express = require("express");
+var app = express();
 var mongoStore = require("connect-mongodb");
 var passive = require("./passive");
 var Page, Entry, LoginToken, User,db;
